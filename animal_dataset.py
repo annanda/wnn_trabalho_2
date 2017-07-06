8de7ea2d7f4a0cda1ab5e5f514d51ba5b1e0d896
Annanda Sousa <annanda.sousa@gmail.com>
2017-07-06 19:59:54 -0300

    Fazendo a opção de balancear as classes no database.

--- animal_dataset.py
+++ animal_dataset.py
@@ -1,15 +1,17 @@
 import csv
-from collections import OrderedDict
+from collections import OrderedDict, Counter
 import rows
 
-VALOR_DIVISAO = 10021
 
 class AnimalDataset:
-    def __init__(self, path_to_dataset):
+    def __init__(self, path_to_dataset, number_exemples=None):
         self.x_train = []
         self.y_train = []
         self.x_test = []
         self.y_test = []
+        self.number_exemples = number_exemples
+        self.number_exemples_test = None
+        self.valor_divisao = None
 
         with open(path_to_dataset, 'r') as csv_file:
             examples = list(csv.reader(csv_file, delimiter=','))
@@ -20,11 +22,20 @@ class AnimalDataset:
                 example = [int(feature) for feature in row[:-1]]
                 x.append(example)
                 y.append(row[-1])
-            self.x_train = x[:VALOR_DIVISAO]
-            self.x_test = x[VALOR_DIVISAO:]
-            self.y_train = y[:VALOR_DIVISAO]
-            self.y_test = y[VALOR_DIVISAO:]
-            # self.transform_test_labels()
+            self.valor_divisao = self.percentage(90, len(y))
+            self.x_train = x[:self.valor_divisao]
+            self.x_test = x[self.valor_divisao:]
+            self.y_train = y[:self.valor_divisao]
+            self.y_test = y[self.valor_divisao:]
+            self.analise_dataset_train()
+            print len(x)
+            print len(self.y_train)
+            # self.balance_dataset_train()
+            # self.balance_dataset_test()
+            # self.analise_dataset_train()
+
+    def percentage(self, percent, whole):
+        return int((percent * whole) / 100.0)
 
     def transform_test_labels(self):
         # classifier = {
@@ -40,6 +51,67 @@ class AnimalDataset:
             y[int(row)] = 1
             new_y_test.append(y)
         self.y_test = new_y_test
+
+    def transform_train_labels(self):
+        new_y_train = []
+        for row in self.y_train:
+            y = [0, 0, 0, 0, 0]
+            y[int(row)] = 1
+            new_y_train.append(y)
+        self.y_train = new_y_train
+
+    def analise_dataset_train(self):
+        exemplos = Counter(self.y_train)
+        print exemplos.most_common(5)
+
+        teste_exemplos = Counter(self.y_test)
+        print teste_exemplos.most_common(5)
+
+    def balance_dataset_train(self):
+        new_x_train = []
+        new_y_train = []
+        if self.number_exemples:
+            number_exemples = self.number_exemples
+        else:
+            contador = Counter(self.y_train)
+            number_exemples = contador.most_common()[-1][-1]
+            self.number_exemples = number_exemples
+        quantidies = {
+            '0': 0,
+            '1': 0,
+            '2': 0,
+            '3': 0,
+            '4': 0}
+        for i, item in enumerate(self.y_train):
+            if quantidies[item] >= number_exemples:
+                continue
+            else :
+                new_x_train.append(self.x_train[i])
+                new_y_train.append(item)
+                quantidies[item] += 1
+        self.x_train = new_x_train
+        self.y_train = new_y_train
+
+    def balance_dataset_test(self):
+        new_x_test = []
+        new_y_test = []
+        contador = Counter(self.y_test)
+        number_exemples = contador.most_common()[-1][-1]
+        quantidies = {
+            '0': 0,
+            '1': 0,
+            '2': 0,
+            '3': 0,
+            '4': 0}
+        for i, item in enumerate(self.y_test):
+            if quantidies[item] >= number_exemples:
+                continue
+            else:
+                new_x_test.append(self.x_test[i])
+                new_y_test.append(item)
+                quantidies[item] += 1
+        self.x_test = new_x_test
+        self.y_test= new_y_test
 
 
 class AnimalTestDataset:
@@ -147,3 +219,12 @@ class AnimalTestDataset:
             table_to.append(row)
 
         rows.export_to_csv(table_to, filename)
+
+if __name__ == '__main__':
+    source_file = 'dataset/new'
+    print '------------ CAT ----------------------'
+    dataset_cat = AnimalDataset(source_file + '_train_cat.csv')
+    print dataset_cat.valor_divisao
+    print '------------ DOG ----------------------'
+    dataset_dog = AnimalDataset(source_file + '_train_dog.csv')
+    print dataset_dog.valor_divisao
