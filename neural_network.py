f381880827c6b09fe76c79d9f69cdffd9ae9ea44
Annanda Sousa <annanda.sousa@gmail.com>
2017-07-06 12:46:54 -0300

    Adicionando resultados

--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,7 @@
 from PyWANN.WiSARD import WiSARD
 from sklearn.metrics import log_loss
 from animal_dataset import AnimalDataset, AnimalTestDataset
+import csv
 
 # source_type = ''
 source_file = 'dataset/new'
@@ -10,35 +11,52 @@ output_file = 'wnn'
 def apply_wisard(source_file, num_bits_addr, bleaching, randomize_positions):
     dataset = AnimalDataset(source_file + '_train_cat.csv')
     cat_testset = AnimalTestDataset(source_file + '_cat_test.csv')
+    bits_memoria_max = 36
+    score_total = []
+    for i in xrange(bits_memoria_max, 16, -1):
+        scores = [i]
+        w = WiSARD(i, bleaching, randomize_positions)
+        w.fit(dataset.x_train + dataset.x_test, dataset.y_train + dataset.y_test)
+        predicted = w.predict_proba(dataset.x_test)
+        # predicted_on_cats = w.predict_proba(cat_testset.x)
+        expected = dataset.y_test
+        score = log_loss(expected, predicted)
+        scores.append(score)
+        print("Logloss on cats: {}, memoria: {}".format(score, i))
 
-    w = WiSARD(num_bits_addr, bleaching, randomize_positions)
-    w.fit(dataset.x_train + dataset.x_test, dataset.y_train + dataset.y_test)
-    # predicted = w.predict_proba(dataset.x_test)
-    predicted_on_cats = w.predict_proba(cat_testset.x)
-    # expected = dataset.y_test
-    # score = log_loss(expected, predicted)
+        ###########################################
 
-    # print("Logloss on cats: {}".format(score))
+        dataset = AnimalDataset(source_file + '_train_dog.csv')
+        dog_testset = AnimalTestDataset(source_file + '_dog_test.csv')
 
-    ###########################################
+        w_2 = WiSARD(num_bits_addr, bleaching, randomize_positions)
+        w_2.fit(dataset.x_train + dataset.x_test, dataset.y_train + dataset.y_test)
+        predicted = w_2.predict_proba(dataset.x_test)
+        # predicted_on_dogs = w_2.predict_proba(dog_testset.x)
+        expected = dataset.y_test
+        score = log_loss(expected, predicted)
+        scores.append(score)
+        print("Logloss on dogs: {}, memoria: {}".format(score, i))
 
-    dataset = AnimalDataset(source_file + '_train_dog.csv')
-    dog_testset = AnimalTestDataset(source_file + '_dog_test.csv')
-
-    w_2 = WiSARD(num_bits_addr, bleaching, randomize_positions)
-    w_2.fit(dataset.x_train + dataset.x_test, dataset.y_train + dataset.y_test)
-    # predicted = w_2.predict_proba(dataset.x_test)
-    predicted_on_dogs = w_2.predict_proba(dog_testset.x)
-    # expected = dataset.y_test
-    # score = log_loss(expected, predicted)
-    # print("Logloss on dogs: {}".format(score))
-
-    dog_testset.export_prob_predictions_to_csv(output_file + '_3' + '.csv', cat_testset.ids + dog_testset.ids,
-                                               list(predicted_on_cats) + list(predicted_on_dogs))
+        # dog_testset.export_prob_predictions_to_csv(output_file + '_memoria_' + str(i) + '.csv', cat_testset.ids + dog_testset.ids,
+        #
+        #                                       list(predicted_on_cats) + list(predicted_on_dogs))
+    #
+        score_total.append(scores)
+    return score_total
 
 
 if __name__ == '__main__':
     num_bits_addr = 34
     bleaching = True
     randomize_positions = True
-    apply_wisard(source_file, num_bits_addr, bleaching, randomize_positions)
+    scores = apply_wisard(source_file, num_bits_addr, bleaching, randomize_positions)
+
+    with open('resultado_memoria_2.csv', 'w') as csv_file:
+        for score in scores:
+            rowwriter = csv.writer(csv_file, delimiter=',')
+            rowwriter.writerow(score)
+
+
+
+
--- results.md
+++ results.md
@@ -3,3 +3,35 @@ memoria: 32 bits
 Bleaching True
 Logloss on cats: 1.75524577218
 Logloss on dogs: 1.55662009371
+
+Logloss on cats: 1.60496611049, memoria: 36
+Logloss on dogs: 1.53746879003, memoria: 36
+Logloss on cats: 1.53535010119, memoria: 35
+Logloss on dogs: 1.53746879003, memoria: 35
+Logloss on cats: 1.53746879003, memoria: 34
+Logloss on dogs: 1.53746879003, memoria: 34
+Logloss on cats: 1.53118124008, memoria: 33
+Logloss on dogs: 1.53746879003, memoria: 33
+Logloss on cats: 1.53083018225, memoria: 32
+Logloss on dogs: 1.53746879003, memoria: 32
+Logloss on cats: 1.53060819143, memoria: 31
+Logloss on dogs: 1.53746879003, memoria: 31
+Logloss on cats: 1.5416795216, memoria: 30
+Logloss on dogs: 1.53746879003, memoria: 30
+Logloss on cats: 1.58435475594, memoria: 29
+Logloss on dogs: 1.53746879003, memoria: 29
+Logloss on cats: 1.57136059848, memoria: 28
+Logloss on dogs: 1.53746879003, memoria: 28
+Logloss on cats: 1.52418494537, memoria: 27
+Logloss on dogs: 1.53746879003, memoria: 27
+Logloss on cats: 1.56796815442, memoria: 26
+Logloss on dogs: 1.53746879003, memoria: 26
+Logloss on cats: 2.49904050249, memoria: 25
+Logloss on dogs: 1.53746879003, memoria: 25
+Logloss on cats: 2.55543870304, memoria: 24
+Logloss on dogs: 1.53746879003, memoria: 24
+Logloss on cats: 3.89375384053, memoria: 23
+Logloss on dogs: 1.53746879003, memoria: 23
+Logloss on cats: 4.29311921721, memoria: 22
+Logloss on dogs: 1.53746879003, memoria: 22
+Logloss on cats: 4.47204766451, memoria: 21
